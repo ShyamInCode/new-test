fcdf9697d5f98c1d11426da486b295bd0a8d9d9e
PATNALA SHYAM RAM <93208535+ShyamInCode@users.noreply.github.com>
2024-10-24 01:54:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ let globalExtractedData = [];
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
