44d4eff4e0e806902414d9e2b64c89d1ed386350
PATNALA SHYAM RAM <93208535+ShyamInCode@users.noreply.github.com>
2024-10-24 00:03:32 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const loginBtn = document.getElementById('loginBtn');
 
     loginBtn.addEventListener('click', () => {
-        window.location.href = `${process.env.LOGIN_URL}`;
+        window.location.href = 'https://new-test-q5fo.onrender.com/login';
     });
 
 });
